689886fa38a6aaae991d33a6e50ed62bfa3e7cf6
Santi Susanti <43363256+saantisusanti@users.noreply.github.com>
2018-09-23 00:18:03 +0700

    Add files via upload

---
+++ PBO3_10117098_Latihan9_Teks/src/pbo3_10117098_latihan9_teks/PBO3_10117098_Latihan9_Teks.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3_10117098_latihan9_teks;
+
+/**
+ *
+ * @author 
+ * NAMA  : Santi Susanti
+ * KELAS : PBO3
+ * NIM   : 10117098
+ */
+public class PBO3_10117098_Latihan9_Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char: " + tab + "berisi " + c);
+    }
+    
+}
